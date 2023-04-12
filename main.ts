d23f08cc987711bebd358b40377b29fddad021af
Evagp <gonzalez@uma.es>
2023-04-13 01:16:13 +0200

    Recta + Crea equipo 1-4

--- main.ts
+++ main.ts
@@ -3,6 +3,13 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
     maqueen.motorStop(maqueen.Motors.All)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 4))
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 4))
+})
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
 	
 })
